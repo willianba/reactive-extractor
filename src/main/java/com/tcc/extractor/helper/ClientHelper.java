9d9c12e25c2a70a7f5aca16b4252ecbfae4459b6
Willian Alves <wba.alves@gmail.com>
2020-10-23 01:54:21 -0300

    add an ugly way of generalize things
    with this ugly piece of code I can pass a repository, a folder, or a file within the request instead of just a repository

--- src/main/java/com/tcc/extractor/helper/ClientHelper.java
+++ src/main/java/com/tcc/extractor/helper/ClientHelper.java
@@ -11,28 +11,56 @@ import reactor.core.publisher.Mono;
 @Component
 public class ClientHelper {
 
+  private static final String FILE = "blob";
+  private static final String DIRECTORY = "tree";
+
   public Flux<String> getDirectoryApiUrl(Flux<String> urls) {
     return urls
       .flatMap(this::getUserAndRepo)
       .flatMap(this::getDirectoryFormattedApiUrl);
   }
 
-  private Mono<String[]> getUserAndRepo(String gitUrl) {
+  private Mono<String> getUserAndRepo(String gitUrl) {
     Pattern pattern = Pattern.compile("\\.com/(.*)");
     Matcher matcher = pattern.matcher(gitUrl);
     matcher.find(); // TODO remove, bad practice
-    return Mono.just(matcher.group(1).split("/"));
+    return Mono.just(matcher.group(1));
   }
 
-  private Mono<String> getDirectoryFormattedApiUrl(String[] userAndRepo) {
+  private Mono<String> getDirectoryFormattedApiUrl(String repoPath) {
     StringBuilder sb = new StringBuilder();
-    String user = userAndRepo[0];
-    String repo = userAndRepo[1];
-    String formattedUrl = sb.append(user)
+    String[] urlParameters = getUrlParameters(repoPath);
+    String formattedUrl = sb.append(urlParameters[0])
       .append("/")
-      .append(repo)
-      .append("/contents")
+      .append(urlParameters[1])
+      .append("/contents/")
+      .append(urlParameters[2])
       .toString();
     return Mono.just(formattedUrl);
+  }
+
+  private String[] getUrlParameters(String repoPath) {
+    String[] splittedPath = repoPath.split("/");
+    if (repoPath.contains(FILE)) {
+      return new String[] {
+        splittedPath[0],
+        splittedPath[1],
+        splittedPath[4] + "/" + splittedPath[5] // ugly af but no time brother, also tighy coupled to my repo
+      };
+    }
+
+    if (repoPath.contains(DIRECTORY)) {
+      return new String[] {
+        splittedPath[0],
+        splittedPath[1],
+        splittedPath[4]
+      };
+    }
+
+    return new String[] { // array for a root's repo url
+      splittedPath[0],
+      splittedPath[1],
+      ""
+    };
   }
 }
